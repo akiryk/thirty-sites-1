806b45314e367942e6c10cfd00f5485d7123b467
adamkiryk <adamkiryk@gmail.com>
2018-02-07 22:23:14 -0500

    Skater image, no scroll cycling

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,50 @@
-// Include gulp
-const gulp = require('gulp');
+const gulp              = require('gulp'),
+      autoprefixer      = require('gulp-autoprefixer'),
+      babel             = require('gulp-babel'),
+      browserSync       = require('browser-sync').create(),
+      gulpif            = require('gulp-if'),
+      sass              = require('gulp-sass'),
+      sourcemaps        = require('gulp-sourcemaps'),
+      uglify            = require('gulp-uglify'),
+      useref            = require('gulp-useref'), // concatenates files
+      watch             = require('gulp-watch'),
+      reload            = browserSync.reload;
 
-// Include Our Plugins
-const babel = require('gulp-babel');
-const jshint = require('gulp-jshint');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const htmlclean = require('gulp-htmlclean');
-const livereload = require('gulp-livereload');
-const newer = require('gulp-newer');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
-const webserver = require('gulp-webserver');
-const devBuild = true;
-
-// HTML processing
-gulp.task('html', function() {
-  var
-    page = gulp.src('src/*.html')
-      .pipe(newer('docs/'));
-
-  // minify production code
-  if (!devBuild) {
-    page = page.pipe(htmlclean());
-  }
-
-  return page
-    .pipe(gulp.dest('docs/'))
-    .pipe(livereload());
-});
-
-// Compile Our Sass
-gulp.task('sass', function() {
-  return gulp.src('src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('docs/css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts', () =>
-  gulp.src([
-    'src/js/vendors/gsap/TweenMax.min.js',
-    'src/js/vendors/gsap/TimelineMax.min.js',
-    'src/js/vendors/ScrollMagic/ScrollMagic.min.js',
-    'src/js/vendors/ScrollMagic/animation.gsap.min.js',
-  ])
-    .pipe(concat('vendors.js'))
-    .pipe(gulp.dest('docs/js'))
-);
-
-gulp.task('webserver', () =>
-  gulp.src('docs')
-    .pipe(webserver({
-      livereload: true,
-      open: true,
-      port: 8000,
-    }))
-);
-
-gulp.task('babel', () =>
-  gulp.src('src/js/main.js')
+gulp.task('css',() => (
+  gulp.src('src/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(babel({
-        presets: ['es2015']
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('docs/js/'))
-    .pipe(livereload())
+    .pipe(sass({ outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('docs/css'))
+    .pipe(browserSync.stream())
+  )
 );
 
-gulp.task('watch', ['webserver'], function() {
-  livereload.listen();
-  gulp.watch('src/js/**/*.js', ['babel']);
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/*.html', ['html'])
+gulp.task('copy', () => (
+  gulp.src('src/**/*.+(html|js)')
+    .pipe(useref())
+    .pipe(gulpif('*.js', sourcemaps.init()))
+    .pipe(gulpif('*.js', babel({presets: ["env"]})))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.js', sourcemaps.write('.')))
+    .pipe(gulp.dest('docs'))
+    .pipe(browserSync.stream())
+))
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "docs"
+        }
+    });
 });
 
-// Default Task
-// gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-gulp.task('default', ['sass', 'babel', 'html', 'watch']);
+gulp.task('default', () => {
+
+  browserSync.init({
+      server: "docs"
+  });
+
+  gulp.watch('src/scss/**/*.scss', ['css']);
+  gulp.watch('src/**/*.+(html|js)', ['copy']);
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,194 +1,175 @@
-const scrollScripts = {
+// TODO: use promises or some other better way to handle how initScrollMagic
+// is called. Right now, it's done in the Pixi setup method.
+(function() {
 
-  init() {
-    t``
-    // this.watchScrolling();
-    this.initScrollMagic();
-  },
+  // Constants
+  const FILTERX     = 250,
+        FILTERY     = 140,
+        APPWIDTH    = 1200,
+        APPHEIGHT   = 1200,
+        APPBGCOLOR  = 0x000000;
 
-  initScrollMagic() {
+  const removeLoader = () => {
+    document.body.classList.add('remove-loader');
+  }
 
-    this.intro = document.getElementById('head1');
-    this.startY = utils.getYPosition(this.intro);
-    this.clone = document.getElementById('head2');
-    this.controller = new ScrollMagic.Controller();
+  const App = {
+    pixiApp: null,
+    displacementFilter: null,
+    pixiImage: 'images/skater-1024.jpg',
+    displacementImage: 'images/clouds-1024.jpg',
 
-    const controller = this.controller,
-          intro = this.intro,
-          startY = this.startY,
-          clone = this.clone;
+    init() {
+      this.createPixiApp();
+      this.loadPixiImage();
+    },
 
-    this.top = document.querySelector('.headline--top');
-    this.bottom = document.querySelector('.headline--bottom');
-    const top = this.top;
-    const bottom = this.bottom;
+    createPixiApp() {
+      this.pixiApp = new PIXI.Application({
+        width: APPWIDTH,
+        height: APPHEIGHT,
+        backgroundColor : APPBGCOLOR,
+        transparent: false,
+        antialias: false,
+      });
+      document.getElementById('display').appendChild(this.pixiApp.view);
+    },
 
-    const duration = .85 * window.innerHeight;
+    /**
+     * Load images for Pixi animations
+     * Keep a reference to this.pixiImage for later manipulations
+     */
+    loadPixiImage() {
+      PIXI.loader
+        .add('mainPic', this.pixiImage)
+        .add('clouds', this.displacementImage)
+        .load(this.setup.bind(this));
+    },
 
-    this.handleEndTitle();
+    /**
+     * Setup is a callback function from PIXI's load method and
+     * it fires once images are ready to handle.
+     */
+    setup(loader, resources){
+      // Fade out the loader animation
+      removeLoader();
 
-    const headingWrapper = new ScrollMagic.Scene({
-      duration: duration,
-    })
-      .setTween('#head1-wrapper', 1, {rotation: 20})
-      .on('update', function(evt){
-      })
-      .addTo(controller);
+      // Cache some references
+      const app = this.pixiApp;
+      const stage = this.pixiApp.stage;
 
-    const startTitleTop = new ScrollMagic.Scene({
-      duration: duration,
-    })
-      .setTween(top, 1, {x: 200 })
-      .addTo(controller);
+      // Cache stage details
+      const centerStageX = app.renderer.screen.width/2;
+      const centerStageY = app.renderer.screen.height/2;
 
-    const startTitleBottom = new ScrollMagic.Scene({
-      duration: duration,
-    })
-      .setTween(bottom, 1, {x: -200 })
-      .addTo(controller);
+      // Make sprites from images
+      const mainPic = new PIXI.Sprite(resources.mainPic.texture);
+      const clouds = new PIXI.Sprite(resources.clouds.texture);
 
-    const scaleObj = bgAnimation.displacementFilter.scale;
-    const blurTween = TweenMax.to(scaleObj, .85, {x: 25, y: 25, ease: Power1.easeOutIn});
+      // Center sprite origins
+      mainPic.anchor.set(.5);
+      clouds.anchor.set(.5);
+      mainPic.position.set(centerStageX, centerStageY);
+      clouds.position.set(centerStageX, centerStageY);
 
-    const copyScene = new ScrollMagic.Scene({
-      offset: 300,
-    })
-      .setTween(blurTween)
-      .addTo(controller);
+      // Make Filters
+      this.addDisplacementFilter(clouds);
 
-    const firstParagraph = document.querySelector('.para');
-    const matrixTween = TweenMax.to(firstParagraph, 1, {transform: 'translate(-3%, 0%) matrix(1,0,0,1,0,0)'});
-    const copy = document.querySelector('.copy');
-    const paragraphScene = new ScrollMagic.Scene({
-      duration: 2000,
-      triggerElement: copy,
-    })
-      .setTween(matrixTween)
-      .addTo(controller);
+      // Add Images
+      stage.addChild(mainPic);
+      stage.addChild(clouds);
 
-  },
+      this.startPixiLoop(clouds);
+      this.initScrollMagic();
+    },
 
-  handleEndTitle() {
-    const controller = this.controller;
-    const clone = this.clone;
-    const startY = this.startY;
-    const top = this.top;
-    const bottom = this.bottom;
+    startPixiLoop(sprite) {
+      // Displacement map animation loop
+      this.pixiApp.ticker.add(delta => gameloop(delta));
 
-    const endTitle = new ScrollMagic.Scene({
-      triggerElement: '#head2',
-    })
-      .addTo(controller); // assign the scene to the controller
-
-    endTitle.on("update", function (event) {
-      // when clone moves past location or original, scroll to top
-      const cloneY = clone.getBoundingClientRect().top;
-      if (cloneY <= startY){
-        TweenMax.set('#head1-wrapper', {rotation: 0});
-        TweenMax.set(top, {x: 0});
-        TweenMax.set(bottom, {x: 0});
-        window.scroll(startY - cloneY,0);
+      function gameloop(delta){
+        sprite.rotation -= .002;
       }
-    });
+    },
 
-  },
-}
+    addDisplacementFilter(img) {
+      this.displacementFilter = new PIXI.filters.DisplacementFilter(img);
+      this.displacementFilter.scale.x = FILTERX;
+      this.displacementFilter.scale.y = FILTERY;
+      this.pixiApp.stage.filters = [this.displacementFilter];
+    },
 
-const bgAnimation = {
+    initScrollMagic() {
+      this.makeController();
+      this.makeHeaderScenes();
+      this.makeParagraphScenes();
+      this.makeDisplacementAnimationScene();
+    },
 
-  app: null,
+    makeController() {
+      this.controller = new ScrollMagic.Controller();
+    },
 
-  displacementFilter: null,
+    makeHeaderScenes() {
+      const intro = document.getElementById('introHead');
+      const top = document.querySelector('.headline--top');
+      const bottom = document.querySelector('.headline--bottom');
+      const duration = .85 * window.innerHeight;
+      const controller = this.controller;
 
-  stage: null,
+      new ScrollMagic.Scene({ duration: duration })
+        .setTween(intro, 1, {rotation: 20})
+        .addTo(controller);
 
-  init({pixiImage}) {
+      new ScrollMagic.Scene({ duration: duration })
+        .setTween(top, 1, {x: 200 })
+        .addTo(controller);
 
-    this.image = pixiImage;
+      new ScrollMagic.Scene({ duration: duration })
+        .setTween(bottom, 1, {x: -200 })
+        .addTo(controller);
+    },
 
-    this.app = new PIXI.Application({
-      width: 1200,
-      height: 1200,
-      transparent: false,
-      antialias: false,
-    });
+    makeParagraphScenes() {
+      const paras = Array.from(document.querySelectorAll('.para'));
+      const controller = this.controller;
 
-    document.getElementById('display').appendChild(this.app.view);
+      paras.forEach(para => {
+        const tween = TweenLite.to(para, 1, {transform: 'matrix(.98, -.075, .075, .98, 0, 0)'});
+        const scene = new ScrollMagic.Scene({
+          duration: '150%',
+          triggerHook: 'onEnter',
+          triggerElement: para,
+        })
+          .setTween(tween)
+          .addTo(controller);
+      })
+    },
 
-    this.stage = this.app.stage;
-    this.loadPixiImage();
-  },
+    makeDisplacementAnimationScene() {
+      const scaleObj = this.displacementFilter.scale;
+      const distortionTween = TweenMax.to(scaleObj, .85, {x: 0, y: 0});
+      const controller = this.controller;
+      const clone = document.getElementById('introHeadClone');
 
-  createDisplacementFilter(img){
-    this.displacementFilter = new PIXI.filters.DisplacementFilter(img);
-    this.displacementFilter.scale.x = 250;
-    this.displacementFilter.scale.y = 140;
-  },
+      new ScrollMagic.Scene({
+        offset: 300,
+        duration: '100%',
+      })
+        .setTween(distortionTween)
+        .addTo(controller);
 
-  loadPixiImage() {
-    PIXI.loader
-      .add('sprite', this.image)
-      .add('filter', 'images/clouds-600.jpg')
-      .load(this.setup.bind(this));
-  },
+      new ScrollMagic.Scene({
+        triggerElement: clone,
+        triggerHook: 'onEnter',
+        duration: '100%',
+      })
+        .setTween(TweenLite.to(scaleObj, 1, {x: FILTERX, y: FILTERY}))
+        .addTo(controller);
+    },
 
-  setup(loader, resources){
-    const app = this.app;
-    const stage = this.stage;
+  }
 
-    // Cache stage details
-    const centerStageX = app.renderer.screen.width/2;
-    const centerStageY = app.renderer.screen.height/2;
+  App.init();
 
-    // Images
-    const sprite = new PIXI.Sprite(resources.sprite.texture);
-    sprite.anchor.set(.5);
-    sprite.position.set(centerStageX, centerStageY);
-    const filterImg = new PIXI.Sprite(resources.filter.texture);
-    filterImg.anchor.set(.5);
-    filterImg.position.set(centerStageX, centerStageY);
-
-    const spriteRightBound = centerStageX + sprite.width/2;
-
-    // Filters
-    // const disFilter = new filters.DisplacementFilter(filterImg);
-    this.createDisplacementFilter(filterImg);
-    filterImg.texture.baseTexture.wrapMode=PIXI.WRAP_MODES.REPEAT;
-
-    stage.filters = [this.displacementFilter];
-
-    // Add Images
-    stage.addChild(sprite);
-    stage.addChild(filterImg);
-
-    app.ticker.add(delta => gameloop(delta));
-
-    function gameloop(delta){
-      filterImg.rotation -= .002;
-    }
-
-    // Start ScrollMagic
-    scrollScripts.init();
-  },
-
-}
-
-const utils = {
-
-  /**
-   * @description Get the y position of an element
-   * @param {element}
-   * @return {number} yPos
-   */
-  getYPosition(el) {
-    let yPos = 0;
-    while(el) {
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-      el = el.offsetParent;
-    }
-    return yPos;
-  },
-
-}
-
-bgAnimation.init({pixiImage: 'images/hamp-600.jpg'});
+})();
